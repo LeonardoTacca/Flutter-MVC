7c3a2511bab50dbbdd907b9f1544167e24adc32a
LeonardoTacca <leotacca@hotmail.com>
2021-08-24 17:41:36 -0300

    successfull firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mvc/Login/Login_Controller.dart';
 
-void main() {
-  runApp(Home());
+import 'package:get/get.dart';
+
+import 'Splash/Login/Splash_View.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(GetMaterialApp(
+      debugShowCheckedModeBanner: false, title: 'Padrão MVC', home: Splash()));
 }
